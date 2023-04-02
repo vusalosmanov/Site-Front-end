03c050ec823c3f8a45e0fdc0d56aceb3930b10f6
vusalosmanov <vusal.osmanov66@gmail.com>
2023-04-02 12:28:23 +0400

    first commit

---
+++ Site3 new year/style.js
@@ -0,0 +1,25 @@
+let myDiv = document.getElementById("my-div");
+
+function isTouchDevice() {
+    try {
+        document.createEvent("TouchEvent");
+        return true;
+    }   catch (e) {
+        return false;
+    }
+}
+const move = (e) => {
+    try {
+        var x = !isTouchDevice() ? e.pageX : e.touches[0].pageX
+        var y = !isTouchDevice() ? e.pageY : e.touches[0].pageY    
+    } catch (e) {}
+    myDiv.style.left = x - 100 + "px";
+    myDiv.style.top = y - 100 + "px";
+};
+document.addEventListener("mousemove", (e) => {
+    move(e);
+});
+document.addEventListener("touchmove", (e) => {
+    move(e);
+});
+
